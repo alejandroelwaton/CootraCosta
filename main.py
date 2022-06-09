17ccc74cbca75d1e70a5ebf2b309629571c1f510
alejandroelwaton <94821128+alejandroelwaton@users.noreply.github.com>
2022-06-08 21:18:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,16 +10,26 @@ riohacha_monteria = Rutas('Riohacha', 'Monteria', 21, 15000)
 santa_marta_barranquilla_diarias = Rutas('Santa marta', 'Barranquilla', 10, 20000)
 rutas = [barranquilla_santa_marta, valledupar_monteria, santa_marta_barranquilla, cartagena_sincelejo, riohacha_monteria, santa_marta_barranquilla_diarias]
 buses = []
+pasajeros = []
 
 def main():
     clear()
     sleep(2)
+    bus = Buses(1, 'ABS', 'Fuena', [[2,2]], barranquilla_santa_marta)
     print('Bienvenido a Cootracosta')
 
     while True:
         inicio_del_programa = int(input('''
 1. Crear un nuevo bus
 2. Modificar un bus
+3. Mostrar buses
+4. Mostrar rutas
+5. Crear ruta
+6. Modificar ruta
+7. Crear pasajero
+8. Mostrar pasajeros
+9. Comprar pasaje
+
 '''))
         if inicio_del_programa == 1:
             nuevo_bus = Buses.anadir_bus(rutas)
@@ -34,9 +44,80 @@ def main():
                     bus_a_modificar = int(input('Bus a modificar: '))
                     if bus_a_modificar == buses.index(bus)+1:
                         bus.modificar_bus(rutas)
+        
+        elif inicio_del_programa == 3:
+            if len(buses) == 0:
+                print('No hay buses creados')
+            else:
+                for bus in buses:
+                    print(f'{buses.index(bus)+1}, {bus.numero}')
+        
+        elif inicio_del_programa == 4:
+            if len(rutas) == 0:
+                print('No hay rutas creadas')
+            else:
+                for ruta in rutas:
+                    print(f'{rutas.index(ruta)+1}, {ruta.origen} - {ruta.destino}')
+        
+        elif inicio_del_programa == 5:
+            nueva_ruta = Rutas.anadir_ruta(rutas)
+            rutas.append(nueva_ruta)
+        
+        elif inicio_del_programa == 6:
+            if len(rutas) == 0:
+                print('No hay rutas creadas')
+            else:
+                for ruta in rutas:
+                    print(f'{rutas.index(ruta)+1}, {ruta.origen} - {ruta.destino}')
+                ruta_a_modificar = int(input('Ruta a modificar: '))
+                if ruta_a_modificar == rutas.index(ruta)+1:
+                    ruta.modificar_ruta(rutas)
+        
+        elif inicio_del_programa == 7:
+            nuevo_pasajero = Pasajero.anadir_pasajero(Pasajero)
+            pasajeros.append(nuevo_pasajero)
+
+        elif inicio_del_programa == 8:
+            if len(pasajeros) == 0:
+                print('No hay pasajeros creados')
+            else:
+                for pasajero in pasajeros:
+                    print(f'{pasajeros.index(pasajero)+1}, {pasajero.nombre}')
+        
+        elif inicio_del_programa == 9:
+            if len(pasajeros) == 0:
+                print('No hay pasajeros creados')
+            else:
+                for pasajero in pasajeros:
+                    print(f'{pasajeros.index(pasajero)+1}, {pasajero.nombre}')
+                pasajero_a_comprar = int(input('Pasajero a comprar: '))
+                for i in pasajeros:
+                    if pasajero_a_comprar == pasajeros.index(i)-1:
+                        i = i
+                    print(pasajero_a_comprar)
+                    fila = int(input('Fila: '))
+                    columna = int(input('Columna: '))
+                    for i in buses:
+                        print('No hay buses disponibles' if len(buses) == 0 else buses.index(i)+1, i.numero)
+                    bus_a_comprar = int(input('Bus a comprar: '))
+                    for i in buses:
+                        if bus_a_comprar == buses.index(i)-1:
+                            bus_a_comprar = i
+                    for i in rutas:
+                        print('No hay rutas disponibles' if len(rutas) == 0 else rutas.index(i)+1, i.origen, i.destino)
+                    ruta = int(input('Ruta a comprar: '))
+                    for i in rutas:
+                        if ruta == rutas.index(i)-1:
+                            ruta = i
+                    i.comprar_asiento(fila, columna, ruta, bus_a_comprar)
+        
 
 
 
 if __name__ == '__main__':
-    main()
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('TRATO DE CERRAR EL PROGRAMA')
 
--- objects.py
+++ objects.py
@@ -1,3 +1,4 @@
+from cffi import VerificationError
 import numpy as np
 import os
 from time import sleep
@@ -14,6 +15,14 @@ def clear():
         print('Tu sistema operativo no es compatible')
     os.system(text)
 
+def verificarSiEsEntero(númeroDigitado: str):
+    # Retorna True o False si es posible la conversión
+    try:
+        int(númeroDigitado)
+        elNúmeroEsEntero = True
+    except:
+        elNúmeroEsEntero = False
+    return elNúmeroEsEntero
 
 
 class Rutas :
@@ -24,36 +33,56 @@ class Rutas :
         self.costo = costo
         self.costo_descuento = costo /2
 
+    def anadir_ruta(self):
+        while True:
+            origen = input('Origen: ')
+            destino = input('Destino: ')
+            horario = int(input('Horario: '))
+            while verificarSiEsEntero(horario) == False and horario > 23:
+                horario = int(input('Horario: '))
+            costo = int(input('Costo: '))
+            while verificarSiEsEntero(costo) == False:
+                costo = int(input('Costo: '))
+
+            nueva_ruta = Rutas(origen, destino, horario, costo)
+            return nueva_ruta
+
         
 
 class Buses:
-    def __init__(self, numero:int, placa:str, conductor:str, mal_estado:list, filas:int = 4, columnas:int =10 , ruta:object = None):
+    def __init__(self, numero:int, placa:str, conductor:str, mal_estado:list, ruta:object = None):
         self.numero = numero
         self.placa = placa
         self.mal_estado = mal_estado 
         self.conductor = conductor
-        self.filas = filas
-        self.columnas = columnas
         self.ruta = ruta
+        self.asientos_vendidos = 0
 
     
     def modificar_bus(self, rutas:list):
         self.numero = int(input('numero del bus: '))
+        while verificarSiEsEntero(self.numero) == False:
+            self.numero = int(input('numero del bus: '))
         self.placa = input('placa del bus: ')
         self.conductor = input('conductor del bus: ')
         i = int(input('Ingrese un numero: '))
+        while verificarSiEsEntero(i) == False:
+            i = int(input('Ingrese un numero: '))
         while i > 0:
             i -= 1
             filas = int(input('Fila del asiento en mal estado: '))
+            while verificarSiEsEntero(filas) == False:
+                filas = int(input('Fila del asiento en mal estado: '))
             columnas = int(input('Columna del asiento en mal estado: '))
+            while verificarSiEsEntero(columnas) == False:
+                columnas = int(input('Columna del asiento en mal estado: '))
             asiento_mal_estado = [[filas, columnas]]
             self.mal_estado.append(asiento_mal_estado)
-        self.filas = int(input('filas del bus: '))
-        self.columnas = int(input('Columnas del bus: '))
-
         for ruta in rutas:
             print(f'{rutas.index(ruta)+1}, {ruta.origen} -> {ruta.destino}')
-        ruta_bus = input('Ruta que seguira el bus: ')
+        ruta_bus = int(input('Ruta que seguira el bus: '))
+        while verificarSiEsEntero(ruta_bus) == False:
+            ruta_bus = int(input('Ruta que seguira el bus: '))
         for ruta in rutas:
             if ruta_bus == rutas.index(ruta)-1:
                 ruta_bus = ruta
@@ -62,28 +91,36 @@ class Buses:
     
 
     def anadir_bus(rutas:list):
-        ID_bus = int(input('numero del bus: '))
+        ID_bus = int(input('numero de identificacion del bus: '))
+        while verificarSiEsEntero(ID_bus) == False:
+            ID_bus = int(input('numero del bus: '))
         placa = input('placa del bus: ')
         conductor = input('conductor del bus: ')
         puestos_mal_estado = int(input('Cantidad de puestos en mal estado: '))
+        while verificarSiEsEntero(puestos_mal_estado) == False:
+            puestos_mal_estado = int(input('Cantidad de puestos en mal estado: '))
         while puestos_mal_estado > 0:
             asientos_mal_estado = []
             filas = int(input('Fila del asiento en mal estado: '))
+            while verificarSiEsEntero(filas) == False:
+                filas = int(input('Fila del asiento en mal estado: '))
             columnas = int(input('Columna del asiento en mal estado: '))
+            while verificarSiEsEntero(columnas) == False:
+                columnas = int(input('Columna del asiento en mal estado: '))
             asientos_mal_estado.append([filas, columnas])
             puestos_mal_estado -= 1
         mal_estado = [asientos for asientos in asientos_mal_estado]
         print(mal_estado)
-        filas = int(input('filas del bus: '))
-        columnas = int(input('Columnas del bus: '))
         for ruta in rutas:
             print(f'{rutas.index(ruta)+1}, {ruta.origen} -> {ruta.destino}')
-        ruta_bus = input('Ruta que seguira el bus: ')
+        ruta_bus = int(input('Ruta que seguira el bus: '))
+        while verificarSiEsEntero(ruta_bus) == False:
+            ruta_bus = int(input('Ruta que seguira el bus: '))
         for ruta in rutas:
             if ruta_bus == rutas.index(ruta)-1:
                 ruta_bus = ruta
 
-        nuevo_bus = Buses(ID_bus, placa, conductor, mal_estado, filas, columnas, ruta)
+        nuevo_bus = Buses(ID_bus, placa, conductor, mal_estado, ruta)
         nuevo_bus.crear_asientos()
         nuevo_bus.asignar_mal_estado()
         nuevo_bus.mostrar_asientos()
@@ -91,10 +128,10 @@ class Buses:
 
     
     def crear_asientos(self):
-        filas = self.filas
-        columnas = self.columnas
+        filas = 4
+        columnas = 10
         #asientos libres por defecto
-        self.asientos = np.zeros((filas,columnas))
+        self.asientos = np.full((filas,columnas), 0, str)
         return self.asientos
 
 
@@ -113,6 +150,8 @@ class Buses:
 
 
     def mostrar_asientos(self):
+        print(self.numero)
+        print('0: Libre\n2: Mal estado\n1: Ocupado')
         print(self.asientos)
         print(f'El conductor del bus {self.conductor} atiende en el bus {self.numero} con placa {self.placa}')
         sleep(2)
@@ -120,13 +159,30 @@ class Buses:
 
 #hasta aqui vamos bien
 class Pasajero:
-    def __init__(self, nombre:str, ID:int, edad:int, hora_comprado:int, puestos:int, efectivo:int):
+    def __init__(self, nombre:str, ID:int, edad:int, hora_comprado:int, efectivo:int, puestos:int=0):
         self.nombre = nombre
         self.apellido = ID
         self.edad = edad
         self.hora_comprado = hora_comprado
         self.puestos = puestos
         self.efectivo = efectivo
+
+    
+    def anadir_pasajero(self):
+        nombre = input('Nombre: ')
+        ID = input('ID: ')
+        edad = int(input('Edad: '))
+        while verificarSiEsEntero(edad) == False:
+            edad = int(input('Edad: '))
+        hora_comprado = int(input('Hora de compra (Formato 24 horas): '))
+        while verificarSiEsEntero(hora_comprado) == False and hora_comprado > 23:
+            hora_comprado = int(input('Hora de compra: '))
+        efectivo = int(input('Efectivo: '))
+        while verificarSiEsEntero(efectivo) == False:
+            efectivo = int(input('Efectivo: '))
+        nuevo_pasajero = Pasajero(nombre, ID, edad, hora_comprado, efectivo)
+        return nuevo_pasajero
+
 
     def mostrar_ruta(self):
         print(f'{self.ruta.origen} - {self.ruta.destino}')
@@ -171,6 +227,7 @@ class Pasajero:
         if self.comprobar_hora(ruta) == True and self.comprobar_edad():
             if bus.comprobar_asientos_libres(fila, columna) == True and self.comprobar_efectivo(ruta) == True and bus.ruta == ruta:
                 self.efectivo -= (ruta.costo_descuento if self.puestos >= 3 else ruta.costo)
+                bus.asientos_vendidos += 1
                 bus.asientos[fila, columna] = 1
                 self.puestos += 1
                 vuelto = f'Su vuelto es  de {self.efectivo}'
@@ -190,5 +247,3 @@ class Pasajero:
         else:
             print('' if self.comprobar_hora(ruta) == True else "No se puede comprar en horario anterior")
             print('' if self.comprobar_edad() == True else "Un menor no puede comprar un asiento")
-
-        
